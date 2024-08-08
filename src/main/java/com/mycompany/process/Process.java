9c10c7bc3683bca5ec1cdae499e227ea6ef4ce9c
sunasterisk.jejercito <jamesandrew.ejercito@sun-asterisk.com>
2024-08-09 01:42:05 +0800

    Process integration

--- src/main/java/com/mycompany/process/Process.java
+++ src/main/java/com/mycompany/process/Process.java
@@ -1,19 +1,67 @@
 package com.mycompany.process;
 
+import java.util.ArrayList;
 
 public class Process {
-    public int id;
-    public int burstTime;
-    public int arrivalTime;
-    public int priority;
+	static ArrayList<Process> processList = new ArrayList<>();
+	
+    private int id;
+    private int burstTime;
+    private int arrivalTime;
+    private int priority;
 
-    public Process(int id, int burstTime, int arrivalTime, int priority) {
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getBurstTime() {
+		return burstTime;
+	}
+
+	public void setBurstTime(int burstTime) {
+		this.burstTime = burstTime;
+	}
+
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setArrivalTime(int arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public Process(int id, int burstTime, int arrivalTime, int priority) {
         this.id = id;
         this.burstTime = burstTime;
         this.arrivalTime = arrivalTime;
         this.priority = priority;
     }
 
+	/**
+	 * Iterates through the processlist and gets their ID's
+	 * @return ArrayList of process ID's
+	 */
+	public static ArrayList<Integer> getProcessArray() {
+		ArrayList<Integer> processes = new ArrayList<Integer>();
+		
+        for (Process process : processList) {
+            processes.add(process.getId());
+        }
+        return processes;
+    }
+	
     @Override
     public String toString() {
         return "Process ID: " + id + ", Burst Time: " + burstTime + ", Arrival Time: " + arrivalTime + ", Priority: " + priority;
